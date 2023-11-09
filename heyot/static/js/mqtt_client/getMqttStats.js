002acea220ad149471040ced62ae70e97d173831
fabiobove-dr <fabio.bove.dr@gmail.com>
2023-11-09 13:54:18 +0100

    ModifiedStyle DevicesStats

--- heyot/static/js/mqtt_client/getMqttStats.js
+++ heyot/static/js/mqtt_client/getMqttStats.js
@@ -5,6 +5,7 @@ function createDeviceCard(device) {
     <path d="M11.5 2a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5Zm2 0a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5Zm-10 8a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1h-6Zm0 2a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1h-6ZM5 3a1 1 0 0 0-1 1h-.5a.5.5 0 0 0 0 1H4v1h-.5a.5.5 0 0 0 0 1H4a1 1 0 0 0 1 1v.5a.5.5 0 0 0 1 0V8h1v.5a.5.5 0 0 0 1 0V8a1 1 0 0 0 1-1h.5a.5.5 0 0 0 0-1H9V5h.5a.5.5 0 0 0 0-1H9a1 1 0 0 0-1-1v-.5a.5.5 0 0 0-1 0V3H6v-.5a.5.5 0 0 0-1 0V3Zm0 1h3v3H5V4Zm6.5 7a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h2a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-2Z"/>\
     <path d="M1 2a2 2 0 0 1 2-2h11a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-2H.5a.5.5 0 0 1-.5-.5v-1A.5.5 0 0 1 .5 9H1V8H.5a.5.5 0 0 1-.5-.5v-1A.5.5 0 0 1 .5 6H1V5H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 2H1Zm1 11a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v11Z"/>\
     </svg>');
+    var loadingSpinner = $('<div>').addClass('spinner-border').addClass('text-light').addClass("small-spinner")
 
     // Create a Bootstrap card element for a device
     var card = $('<div>').addClass('card').addClass("device-stats-card");
@@ -14,7 +15,7 @@ function createDeviceCard(device) {
     cardTitle.prepend(svgIcon);
     var cardText1 = $('<p>').addClass('card-text').text('Connected Devices: ' + device.connected_device_count);
     var cardText2 = $('<p>').addClass('card-text').text('Total Messages: ' + device.total_message_count);
-
+    cardText2.append(loadingSpinner)
     // Append elements to construct the card
     cardBody.append(cardTitle, cardText1, cardText2);
     card.append(cardBody);
