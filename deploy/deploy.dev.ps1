6dddfef924361fc1e8efa4bfcdf93a32abd88835
fabiobove-dr <fabio.bove.dr@gmail.com>
2023-11-09 14:39:50 +0100

    Fixed some Issue

--- deploy/deploy.dev.ps1
+++ deploy/deploy.dev.ps1
@@ -11,6 +11,8 @@ docker-compose -f ..\docker-compose.dev.yml --env-file ..\environment\.env.dev u
 ###################################################################################################################################################################################################
 docker exec -it heyot_web python manage.py makemigrations database_manager --no-input 
 docker exec -it heyot_web python manage.py migrate database_manager --no-input 
+docker exec -it heyot_web python manage.py makemigrations  --no-input 
+docker exec -it heyot_web python manage.py migrate  --no-input 
 docker exec -it heyot_web python manage.py collectstatic --no-input --clear 
 docker exec -it heyot_web python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='$DevelopmentUser').exists() or User.objects.create_superuser('$DevelopmentUser', '$DevelopmentUser', '$DevelopmentUser')"
 ###################################################################################################################################################################################################
